b3cfd0ba5f9756df7db884a9ffdba3395a6e3905
Ryan Brideau <ryan.brideau@gmail.com>
2014-01-14 20:19:45 -0400

    Change names, prepare for salary work.

--- Salaries.R
+++ Salaries.R
@@ -7,20 +7,20 @@ uni.ftes.file <- "Data/CAUBO/20112012/t_D_Rep_0.csv"
 president.salaries <- read.csv(data.file, header=TRUE)
 uni.ftes <- read.csv(uni.ftes.file, header=TRUE)
 
-# Fills in FTEs
-president.salaries$FTEs <- NA
-for (university in as.character(president.salaries$University)) {
-  if (length(uni.ftes[uni.ftes$INSTNAME == university,]$FTE) > 0 ) {
-    president.salaries[president.salaries$University == university,]$FTEs <- uni.ftes[uni.ftes$INSTNAME == university,]$FTE
-  } else {
-    president.salaries[president.salaries$University == university,]$FTEs == NA
-  }
-}
-
-# Write out what was updated so it can be augmented manually
-write.csv(president.salaries, "presidentsalariesupdated.csv", row.names=FALSE)
-
-theme <- theme(plot.title=element_text(size=rel(2)), axis.title.x=element_text(size=rel(1.5)),  axis.title.y=element_text(size=rel(1.5)), axis.text.x=element_text(size=rel(1.5)), axis.text.y=element_text(size=rel(1.5)) )
+# Fills in FTEs - needed initially when cleaning the data
+# president.salaries$FTEs <- NA
+# for (university in as.character(president.salaries$University)) {
+#   if (length(uni.ftes[uni.ftes$INSTNAME == university,]$FTE) > 0 ) {
+#     president.salaries[president.salaries$University == university,]$FTEs <- uni.ftes[uni.ftes$INSTNAME == university,]$FTE
+#   } else {
+#     president.salaries[president.salaries$University == university,]$FTEs == NA
+#   }
+# }
+# 
+# # Write out what was updated so it can be augmented manually
+# write.csv(president.salaries, "presidentsalariesupdated.csv", row.names=FALSE)
+# 
+# theme <- theme(plot.title=element_text(size=rel(2)), axis.title.x=element_text(size=rel(1.5)),  axis.title.y=element_text(size=rel(1.5)), axis.text.x=element_text(size=rel(1.5)), axis.text.y=element_text(size=rel(1.5)) )
 
 library(grid)
 plot <- ggplot(president.salaries, aes(x=reorder(University,Salary), y=Salary, fill=FTEs)) + theme +
