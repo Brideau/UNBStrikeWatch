0964e467d432b04e190ad4e06fcf6ed531d88d85
Ryan Brideau <ryan.brideau@gmail.com>
2014-01-15 02:45:58 -0400

    Fixed a typo that caused Concordia confusion.

--- Salaries.R
+++ Salaries.R
@@ -40,7 +40,7 @@ salary.file.name <- "FacultySalaries"
 years <- 2000:2009
 professor.types <- c("Full professor","Associate professor","Assistant professor","Ranks/level below assistant professor")
 # Data missing for Saskatchewan
-comp.schools <- c("McMaster University", "University of Waterloo", "University of Windsor", "Queen's University", "University of Guelph", "Carleton University", "Simon Fraser University", "University of Regina", "Dalhousie University", "Memorial University of Newfoundland", "University of Manitoba", "Concordia University College of Alberta", "University of Victoria", "University of New Brunswick")
+comp.schools <- c("McMaster University", "University of Waterloo", "University of Windsor", "Queen's University", "University of Guelph", "Carleton University", "Simon Fraser University", "University of Regina", "Dalhousie University", "Memorial University of Newfoundland", "University of Manitoba", "Concordia University", "University of Victoria", "University of New Brunswick")
 
 salary.data <- list()
 for (year in years) {
@@ -83,7 +83,7 @@ salary.data.df$IsUNB <- NA
 salary.data.df$IsUNB <- salary.data.df$University == "University of New Brunswick"
 
 # Sets a common theme across graphs
-theme <- theme(plot.title=element_text(size=rel(2)), axis.title.x=element_text(size=rel(1.5)),  axis.title.y=element_text(size=rel(1.5)), axis.text.x=element_text(size=rel(1.5)), axis.text.y=element_text(size=rel(1.5)), legend.title=element_text(size=rel(2)) )
+theme <- theme(plot.title=element_text(size=rel(1.7)), axis.title.x=element_text(size=rel(1.5)),  axis.title.y=element_text(size=rel(1.5)), axis.text.x=element_text(size=rel(1.5)), axis.text.y=element_text(size=rel(1.5)), legend.text=element_text(size=rel(1.1)) )
 
 # Plot full professor median salaries, nominal
 fullprof.median.nominal <- subset(salary.data.df, Professor.Type == "Full professor" & Salary.Measure == "Median Salary")
@@ -133,7 +133,7 @@ ggsave(filename="Plots/Salaries/FullprofMedian.png", plot=fullprof.median.plot, 
 # Plot associate professor 90th percentile salaries
 associate.90 <- subset(salary.data.df, Professor.Type == "Associate professor" & Salary.Measure == "90th percentile")
 associate.90.plot <- ggplot(associate.90, aes(x=Year, y=Salary2002, colour=University, linetype=(IsUNB))) + 
-  ggtitle("Associate Professors' 90th Percentile Salary 2000-2001 to 2009-2010") +
+  ggtitle("Assoc. Professors' 90th Percentile Salary 2000-2001 to 2009-2010") +
   geom_line(size=1.1) + 
   geom_point(size=2.5) +
   scale_y_continuous(name="Salary (2002 Dollars)", labels=dollar, limits=c(40000,160000)) +
@@ -148,7 +148,7 @@ ggsave(filename="Plots/Salaries/Associate90.png", plot=associate.90.plot, width=
 # Plot associate professor median salaries
 associate.median <- subset(salary.data.df, Professor.Type == "Associate professor" & Salary.Measure == "Median Salary")
 associate.median.plot <- ggplot(associate.median, aes(x=Year, y=Salary2002, colour=University, linetype=(IsUNB))) + 
-  ggtitle("Associate Professors' Median Salary 2000-2001 to 2009-2010") +
+  ggtitle("Assoc. Professors' Median Salary 2000-2001 to 2009-2010") +
   geom_line(size=1.1) + 
   geom_point(size=2.5) +
   scale_y_continuous(name="Salary (2002 Dollars)", labels=dollar, limits=c(40000,160000)) +
@@ -178,7 +178,7 @@ ggsave(filename="Plots/Salaries/AssistantMedian.png", plot=assistant.median.plot
 # Plot assistant professor 10th percentile salaries
 assistant.10 <- subset(salary.data.df, Professor.Type == "Assistant professor" & Salary.Measure == "10th percentile")
 assistant.10.plot <- ggplot(assistant.10, aes(x=Year, y=Salary2002, colour=University, linetype=(IsUNB))) + 
-  ggtitle("Assistant Professors' 10th Percentile Salary 2000-2001 to 2009-2010") +
+  ggtitle("Assist. Professors' 10th Percentile Salary 2000-2001 to 2009-2010") +
   geom_line(size=1.1) + 
   geom_point(size=2.5) +
   scale_y_continuous(name="Salary (2002 Dollars)", labels=dollar, limits=c(40000,160000)) +
